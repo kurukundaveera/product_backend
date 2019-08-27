7fdf750607f5afb53aef5aee18c96f296819af9f
Gurpreet Singh Gohir <gurpreet.gohir@gmail.com>
2019-08-27 17:01:13 +0530

    gurpreet

--- src/main/java/com/hcl/bankproduct/repository/OrderRepository.java
+++ src/main/java/com/hcl/bankproduct/repository/OrderRepository.java
@@ -1,5 +1,8 @@
 package com.hcl.bankproduct.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +15,7 @@ import com.hcl.bankproduct.entity.Orders;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Orders, Integer> {
+	
+	public List<Orders> findByOrderDateGreaterThanAndOrderDateLessThan(LocalDate startDate, LocalDate endDate);
 
 }
